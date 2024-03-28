de557a71f42b936bfa9e1a0d8bd30209e4bba9fe
adrianjz02 <adr.jimenez.ian@gmail.com>
2024-03-28 13:21:12 +0100

    new file:   jo2024/.gitignore 	new file:   jo2024/.mvn/wrapper/maven-wrapper.jar 	new file:   jo2024/.mvn/wrapper/maven-wrapper.properties 	new file:   jo2024/mvnw 	new file:   jo2024/mvnw.cmd 	new file:   jo2024/pom.xml 	new file:   jo2024/src/main/java/com/jeuxolympiques/jo2024/Jo2024Application.java 	new file:   jo2024/src/main/resources/application.properties 	new file:   jo2024/src/test/java/com/jeuxolympiques/jo2024/Jo2024ApplicationTests.java

---
+++ jo2024/src/main/java/com/jeuxolympiques/jo2024/Jo2024Application.java
@@ -0,0 +1,13 @@
+package com.jeuxolympiques.jo2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jo2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jo2024Application.class, args);
+	}
+
+}
---
+++ jo2024/src/test/java/com/jeuxolympiques/jo2024/Jo2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeuxolympiques.jo2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jo2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
