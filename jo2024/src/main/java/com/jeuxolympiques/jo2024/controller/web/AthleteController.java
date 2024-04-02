db289a03a3d3e252da88c7d4bf5c9c5123a844c8
angelhu.pro@gmail.com <angelhu.pro@gmail.com>
2024-04-03 00:23:38 +0200

    Athlete supp

--- jo2024/src/main/java/com/jeuxolympiques/jo2024/controller/web/AthleteController.java
+++ jo2024/src/main/java/com/jeuxolympiques/jo2024/controller/web/AthleteController.java
@@ -2,10 +2,8 @@ package com.jeuxolympiques.jo2024.controller.web;
 
 import com.jeuxolympiques.jo2024.model.Athlete;
 import com.jeuxolympiques.jo2024.repository.AthleteRepository;
-
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +27,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/athletes")
 public class AthleteController {
+
     @Autowired
     private AthleteRepository athleteRepository;
 
@@ -47,7 +46,6 @@ public class AthleteController {
 
     @PostMapping("/add")
     public String addAthlete(@ModelAttribute Athlete athlete) {
-
         athleteRepository.save(athlete);
         return "redirect:/athletes";
     }
@@ -62,4 +60,9 @@ public class AthleteController {
         return "athlete-profile";
     }
 
+    @GetMapping("/delete/{id}")
+    public String deleteAthlete(@PathVariable Long id) {
+        athleteRepository.deleteById(id);
+        return "redirect:/athletes";
+    }
 }
