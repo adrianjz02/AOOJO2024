151d7789055697847229d5df8a3d1451f0dad6e9
angelhu.pro@gmail.com <angelhu.pro@gmail.com>
2024-04-03 00:13:36 +0200

    Option "supprimer"

--- jo2024/src/main/java/com/jeuxolympiques/jo2024/controller/web/CountryController.java
+++ jo2024/src/main/java/com/jeuxolympiques/jo2024/controller/web/CountryController.java
@@ -2,11 +2,13 @@ package com.jeuxolympiques.jo2024.controller.web;
 
 import com.jeuxolympiques.jo2024.model.Country;
 import com.jeuxolympiques.jo2024.repository.CountryRepository;
+import com.jeuxolympiques.jo2024.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,6 +20,9 @@ public class CountryController {
 
     @Autowired
     private CountryRepository countryRepository;
+
+    @Autowired
+    private CountryService countryService;
 
     @GetMapping
     public String getAllCountries(Model model) {
@@ -37,5 +42,10 @@ public class CountryController {
         countryRepository.save(country);
         return "redirect:/countries";
     }
-}
 
+    @GetMapping("/delete/{id}")
+    public String deleteCountry(@PathVariable Long id) {
+        countryService.deleteCountry(id);
+        return "redirect:/countries";
+    }
+}
