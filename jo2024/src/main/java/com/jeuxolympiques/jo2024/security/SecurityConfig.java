979701ac0643ea95b3ae2eceede46135ae1613c3
Nino Fazer <fazernino@gmail.com>
2024-04-03 21:57:59 +0200

    renamed:    jo2024/src/main/java/com/jeuxolympiques/jo2024/controller/web/ControllerAccueil.java -> jo2024/src/main/java/com/jeuxolympiques/jo2024/controller/web/HomeController.java 	modified:   jo2024/src/main/java/com/jeuxolympiques/jo2024/security/SecurityConfig.java

--- jo2024/src/main/java/com/jeuxolympiques/jo2024/security/SecurityConfig.java
+++ jo2024/src/main/java/com/jeuxolympiques/jo2024/security/SecurityConfig.java
@@ -2,25 +2,18 @@ package com.jeuxolympiques.jo2024.security;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
-import com.jeuxolympiques.jo2024.Handler.AuthenticationFailureHandler.InscriptionFailureHandler;
 import com.jeuxolympiques.jo2024.Handler.AuthenticationFailureHandler.LoginFailureHandler;
 
 import org.springframework.security.core.Authentication;
